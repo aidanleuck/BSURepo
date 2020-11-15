aa731b7d3c45d739664340e157dda8e7ae6555f0
aidanleuck <aidan@leucks.com>
2020-11-15 14:25:07 -0700

    List Implementation

--- CS425/P2/list.h
+++ CS425/P2/list.h
@@ -1,80 +1,164 @@
-struct DLL{
-    struct Node* head;
-    struct Node* tail ;
-    
+struct DLL
+{
+    struct Node *head;
+    struct Node *tail;
+
     int count;
 };
 
-struct Node{
+struct Node
+{
     struct Node *next;
-    struct BPHead* val;
+    struct BPHead *val;
     struct Node *prev;
 };
 
-void initializeList(struct DLL* DLLlist){
+void initializeList(struct DLL *DLLlist)
+{
     DLLlist->head = NULL;
     DLLlist->tail = NULL;
     DLLlist->count = 0;
-    
 }
 
-void add(struct DLL* DLLlist, struct BPHead* addSeg){
-     int internalCounter = 0;
-     
-     if(DLLlist->count == 0){
+void add(struct DLL *DLLlist, struct BPHead *addSeg)
+{
+
+    if (DLLlist->count == 0)
+    {
         DLLlist->head = malloc(sizeof(struct Node));
         DLLlist->tail = DLLlist->head;
         DLLlist->head->next = NULL;
         DLLlist->head->prev = NULL;
-
     }
-    else if(DLLlist->count == 1){
+    else if (DLLlist->count == 1)
+    {
         DLLlist->head = malloc(sizeof(struct Node));
         DLLlist->head->next = DLLlist->tail;
-      
+
         DLLlist->tail->prev = DLLlist->head;
         DLLlist->head->prev = NULL;
         DLLlist->tail->next = NULL;
     }
-    else{
-      struct Node* temp = DLLlist->head;
-      DLLlist->head = malloc(sizeof(struct Node));
-      DLLlist->head->next = temp;
-      temp->prev = DLLlist->head;
-      temp->next = DLLlist->tail;
-      DLLlist->tail->prev = temp;
-      
+    else
+    {
+        struct Node *temp = DLLlist->head;
+        DLLlist->head = malloc(sizeof(struct Node));
+        DLLlist->head->next = temp;
+        DLLlist->head->prev = NULL;
+        temp->prev = DLLlist->head;
     }
-     DLLlist->head->val = malloc(sizeof(struct BPHead));
-    *DLLlist->head->val = *addSeg;
-    DLLlist->count = DLLlist->count + 1; 
 
-   
+    DLLlist->head->val = malloc(sizeof(struct BPHead));
+    *DLLlist->head->val = *addSeg;
+    DLLlist->count = DLLlist->count + 1;
 
     //sort((orderedList), count);
-
 }
 /**
  * If for some reason we received out of order we need to just check the list and ensure that it is sorted.
  */
-void sort(int **orderedList, int *count){
-    int *begList = *orderedList;
-    for(int i = 1; i < *count; i++){
-        int key = begList[i];
-        int j = i - 1;
+void sort(struct DLL *list)
+{
 
-    while(j>=0 && begList[j] < key){
-        begList[j+1] = begList[j];
-        j = j-1;
-    }
-    begList[j+1] = key;
+    struct Node *currValue = list->head->next;
+    struct Node *sortPointer = list->head;
 
+    while (currValue != NULL)
+    {
+
+        while (currValue != NULL)
+        {
+            struct Node tempSort = *sortPointer;
+            struct Node tempCurr = *currValue;
+            if (currValue->val->segNum > sortPointer->val->segNum)
+            {
+                printf("%d", currValue->val->segNum);
+
+                if (sortPointer->next != currValue)
+                {
+                    currValue->prev->next = currValue->next;
+                    if (sortPointer == list->head && currValue != list->tail)
+                    {
+                        list->head = currValue;
+                        currValue->next->prev = currValue->prev;
+                    }
+                    else if (currValue == list->tail && sortPointer != list->head)
+                    {
+                        list->tail = currValue->prev;
+                        sortPointer->prev->next = currValue;
+                    }
+                    else if (currValue == list->tail && sortPointer == list->head)
+                    {
+                        list->tail = currValue->prev;
+                        list->head = currValue;
+                    }
+                    else
+                    {
+                        sortPointer->prev->next = currValue;
+
+                        currValue->next->prev = currValue->prev;
+                    }
+                }
+                else
+                {
+                    sortPointer->next = tempCurr.next;
+
+                    if (sortPointer == list->head && currValue != list->tail)
+                    {
+                        list->head = currValue;
+                        currValue->next->prev = sortPointer;
+                    }
+                    else if (currValue == list->tail && sortPointer != list->head)
+                    {
+                        list->tail = tempCurr.prev;
+                        sortPointer->prev->next = currValue;
+                    }
+                    else if (currValue == list->tail && sortPointer == list->head)
+                    {
+                        list->tail = currValue->prev;
+                        list->head = currValue;
+                    }
+                    else
+                    {
+                        sortPointer->prev->next = currValue;
+                    }
+                }
+
+                sortPointer->prev = currValue;
+                currValue->next = sortPointer;
+                currValue->prev = tempSort.prev;
+
+                sortPointer = currValue;
+            }
+
+            currValue = tempCurr.next;
+        }
+        sortPointer = sortPointer->next;
+        currValue = sortPointer->next;
     }
 }
-
-void clearList(int **orderedList, int *count){
-    for(int i = 0; i < *count; i++){
-        free(*(orderedList + i));
+void clearList(struct DLL *list)
+{
+    struct Node *curr = list->head;
+    for (int i = 0; i < list->count; i++)
+    {
+        free(curr->val);
+        struct Node *remove = curr;
+        curr = curr->next;
+        free(remove);
     }
-    *count = 0;
+    free(list->head);
+    free(list->tail);
+
+    list->count = 0;
+}
+
+int inList(struct DLL *list, struct BPHead *addSeg)
+{
+    int found = 0;
+    if (addSeg->segNum <= list->head->val->segNum)
+    {
+        found = 1;
+    }
+    return found;
 }
--- CS425/P2/sender.c
+++ CS425/P2/sender.c
@@ -4,8 +4,9 @@
 
 volatile sig_atomic_t recvFlag = 0;
 
-int main(int argc, char *argv[]){
-    
+int main(int argc, char *argv[])
+{
+
     int endpoint = createSocket();
     struct sockaddr_in sender = createSender(endpoint, argv[1], argv[2]);
     struct sockaddr_in dest = createDest(argv[1], argv[2]);
@@ -18,8 +19,8 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-
-struct sockaddr_in createDest(char *ip, char *destPort){
+struct sockaddr_in createDest(char *ip, char *destPort)
+{
     int destPortNum = atoi(destPort);
     struct sockaddr_in dest;
     memset(&dest, 0, sizeof dest);
@@ -30,7 +31,8 @@ struct sockaddr_in createDest(char *ip, char *destPort){
     return dest;
 }
 
-struct sockaddr_in createSender(int endpoint, char *ip, char *destPort){
+struct sockaddr_in createSender(int endpoint, char *ip, char *destPort)
+{
     struct sockaddr_in sender;
     memset(&sender, 0, sizeof sender);
     sender.sin_family = AF_INET;
@@ -43,168 +45,143 @@ struct sockaddr_in createSender(int endpoint, char *ip, char *destPort){
 /**
  * Binds addressing information for sender
  **/
-void bindSender(int endpoint, struct sockaddr_in sender){
-    bind(endpoint, (struct sockaddr *) &sender, sizeof(sender));
-                   
- }
-  void handle_alarm(int sig){
+void bindSender(int endpoint, struct sockaddr_in sender)
+{
+    bind(endpoint, (struct sockaddr *)&sender, sizeof(sender));
+}
+void handle_alarm(int sig)
+{
     recvFlag = 1;
- }
- 
- void requestACK(int endpoint, struct BPHead *sendHeader, int *unackSegs, struct sockaddr_in dest, struct sockaddr_in sender){
+}
 
-     struct sigaction handler;
-     int length;
-     handler.sa_flags = 0;
-     sigemptyset(&handler.sa_mask);
-     handler.sa_handler = handle_alarm;
-     sigaction(SIGALRM, &handler, NULL);
+void requestACK(int endpoint, struct BPHead *sendHeader, int *unackSegs, struct sockaddr_in dest, struct sockaddr_in sender)
+{
 
-     
-         alarm(10);
-         recvfrom(endpoint, sendHeader, sizeof *sendHeader ,0, (struct sockaddr *) &sender, (socklen_t *) &length );
-         if(recvFlag){
-             recvFlag = 0;
+    struct sigaction handler;
+    int length;
+    handler.sa_flags = 0;
+    sigemptyset(&handler.sa_mask);
+    handler.sa_handler = handle_alarm;
+    sigaction(SIGALRM, &handler, NULL);
 
-             
-             
-        
-     }
- }
+    alarm(10);
+    recvfrom(endpoint, sendHeader, sizeof *sendHeader, 0, (struct sockaddr *)&sender, (socklen_t *)&length);
+    if (recvFlag)
+    {
+        recvFlag = 0;
+    }
+}
 
- void sendDatagram(int endpoint, struct BPHead *sendHeader, struct sockaddr_in dest, struct sockaddr_in sender){
+void sendDatagram(int endpoint, struct BPHead *sendHeader, struct sockaddr_in dest, struct sockaddr_in sender)
+{
 
-     long bfr_len = 4294967295;
-     char *bfr = (char *) malloc(bfr_len);
-     char *curpos = bfr; // pointer to bfr
+    long bfr_len = 4294967295;
+    char *bfr = (char *)malloc(bfr_len);
+    char *curpos = bfr; // pointer to bfr
 
-     
+    int16_t sequenceNum = 0;
 
-     int16_t sequenceNum = 0;
-        
-     struct DLL unacknowledgedSegments; // Keeps track of unacknowledged data segments
-     initializeList(&unacknowledgedSegments);
-     struct BPHead unacknowledgedSegment;
-     unacknowledgedSegment.segNum = 0;
-     struct BPHead unacknowledgedSegment1;
-     unacknowledgedSegment1.segNum = 1;
-     struct BPHead unacknowledgedSegment2;
-     unacknowledgedSegment2.segNum = 2;
-     add(&unacknowledgedSegments, &unacknowledgedSegment);
-     add( &unacknowledgedSegments, &unacknowledgedSegment1);
-     add( &unacknowledgedSegments, &unacknowledgedSegment2);
+    struct DLL unacknowledgedSegments; // Keeps track of unacknowledged data segments
+    initializeList(&unacknowledgedSegments);
+    struct BPHead unacknowledgedSegment;
+    unacknowledgedSegment.segNum = 9;
+    struct BPHead unacknowledgedSegment1;
+    unacknowledgedSegment1.segNum = 7;
+    struct BPHead unacknowledgedSegment2;
+    unacknowledgedSegment2.segNum = 5;
 
-    
-     
-    
+    struct BPHead unacknowledgedSegment3;
+    unacknowledgedSegment3.segNum = 12;
 
-     
-     
-   
-         freopen(NULL, "rb", stdin); // Read the actual binary data of the file
-         size_t numBytes = fread(bfr, 1, bfr_len, stdin); // Reads in file byte by byte
-         long fileSize = numBytes;       // Save size of the file read
-         long fileLeft = fileSize;       // Keep track how much file left to send
+    struct BPHead unacknowledgedSegment4;
+    unacknowledgedSegment4.segNum = 1;
 
-         
-            while(fileLeft > 0){
-                if(fileLeft >= 512){ // If there is more than 512 bytes left to transfer
-                memcpy(&sendHeader->data, curpos, maxPayLoad);
-                sendHeader->flag.bits.DAT = 1;
-                sendHeader->size = maxPayLoad;
-                sendHeader->segNum = sequenceNum;
-                
-                sendto(endpoint, sendHeader, sizeof *sendHeader, 0,(const struct sockaddr*) &dest, sizeof(dest));
+    add(&unacknowledgedSegments, &unacknowledgedSegment);
+    sort(&unacknowledgedSegments);
 
-                sequenceNum++;
-                sendHeader->window = sendHeader->window - 1;
-             
-                
-                //requestACK(endpoint, sendHeader, unacknowledgedSegment, dest, sender);
+    freopen(NULL, "rb", stdin);                      // Read the actual binary data of the file
+    size_t numBytes = fread(bfr, 1, bfr_len, stdin); // Reads in file byte by byte
+    long fileSize = numBytes;                        // Save size of the file read
+    long fileLeft = fileSize;                        // Keep track how much file left to send
 
-            
-                
-            
-                fileLeft -= maxPayLoad;
-                curpos += maxPayLoad;
-                 }
-                 else if(fileLeft < 512 && fileLeft > 0){ // If there are less than 512 bytes left, only send remaining data
-                     sendHeader->size = fileLeft;
-                     sendHeader->flag.bits.DAT = 1;
-                     sendHeader->segNum = sequenceNum;
-                     memcpy(&sendHeader->data, curpos, fileLeft);
-                     
-                     sendto(endpoint, sendHeader, HEADER_SIZE + fileLeft, 0,(const struct sockaddr*) &dest, sizeof(dest));
-                     sequenceNum++;
-                     sendHeader->window = sendHeader->window - 1;
-            
-                     //requestACK(endpoint, sendHeader, unacknowledgedSegment, dest, sender);
+    while (fileLeft > 0)
+    {
+        if (fileLeft >= 512)
+        { // If there is more than 512 bytes left to transfer
+            memcpy(&sendHeader->data, curpos, maxPayLoad);
+            sendHeader->flag.bits.DAT = 1;
+            sendHeader->size = maxPayLoad;
+            sendHeader->segNum = sequenceNum;
 
-                     
-                     
-                     
-                     fileLeft -= maxPayLoad;
-                     curpos += maxPayLoad;
-                    
-                 }
-                  memset(sendHeader,0, sizeof *sendHeader); // Reset the header to default state
-                  
-                
-             }
-                
-                 sendHeader->flag.bits.EOM = 1;
-                 
-                 sendto(endpoint, sendHeader, HEADER_SIZE, 0,(const struct sockaddr*) &dest, sizeof(dest));
-                 free(bfr);
- }
+            sendto(endpoint, sendHeader, sizeof *sendHeader, 0, (const struct sockaddr *)&dest, sizeof(dest));
 
+            sequenceNum++;
+            sendHeader->window = sendHeader->window - 1;
 
+            //requestACK(endpoint, sendHeader, unacknowledgedSegment, dest, sender);
 
-struct BPHead sendRWA( int endpoint,struct sockaddr_in dest, struct sockaddr_in sender){
+            fileLeft -= maxPayLoad;
+            curpos += maxPayLoad;
+        }
+        else if (fileLeft < 512 && fileLeft > 0)
+        { // If there are less than 512 bytes left, only send remaining data
+            sendHeader->size = fileLeft;
+            sendHeader->flag.bits.DAT = 1;
+            sendHeader->segNum = sequenceNum;
+            memcpy(&sendHeader->data, curpos, fileLeft);
 
-     struct BPHead sendHeader;
-     struct BPHead recvHeader;
-    
-     struct sigaction handler;
-     
-     handler.sa_flags = 0;
-     sigemptyset(&handler.sa_mask);
-     handler.sa_handler = handle_alarm;
+            sendto(endpoint, sendHeader, HEADER_SIZE + fileLeft, 0, (const struct sockaddr *)&dest, sizeof(dest));
+            sequenceNum++;
+            sendHeader->window = sendHeader->window - 1;
 
-     memset(&sendHeader,0, sizeof sendHeader);
-     memset(&recvHeader,0, sizeof sendHeader);
+            //requestACK(endpoint, sendHeader, unacknowledgedSegment, dest, sender);
 
-     
+            fileLeft -= maxPayLoad;
+            curpos += maxPayLoad;
+        }
+        memset(sendHeader, 0, sizeof *sendHeader); // Reset the header to default state
+    }
 
-     int length;
-     sendHeader.window = 0;
-     sendHeader.flag.bits.RWA = 1;
-     
-     sigaction(SIGALRM, &handler, NULL);
-     
-     sendto(endpoint, &sendHeader, HEADER_SIZE, 0,(const struct sockaddr*) &dest, sizeof(dest));
-    
-    while(recvHeader.window == 0){
+    sendHeader->flag.bits.EOM = 1;
 
-        if(recvFlag){
-            sendto(endpoint, &sendHeader, HEADER_SIZE, 0,(const struct sockaddr*) &dest, sizeof(dest));
+    sendto(endpoint, sendHeader, HEADER_SIZE, 0, (const struct sockaddr *)&dest, sizeof(dest));
+    free(bfr);
+}
+
+struct BPHead sendRWA(int endpoint, struct sockaddr_in dest, struct sockaddr_in sender)
+{
+
+    struct BPHead sendHeader;
+    struct BPHead recvHeader;
+
+    struct sigaction handler;
+
+    handler.sa_flags = 0;
+    sigemptyset(&handler.sa_mask);
+    handler.sa_handler = handle_alarm;
+
+    memset(&sendHeader, 0, sizeof sendHeader);
+    memset(&recvHeader, 0, sizeof sendHeader);
+
+    int length;
+    sendHeader.window = 0;
+    sendHeader.flag.bits.RWA = 1;
+
+    sigaction(SIGALRM, &handler, NULL);
+
+    sendto(endpoint, &sendHeader, HEADER_SIZE, 0, (const struct sockaddr *)&dest, sizeof(dest));
+
+    while (recvHeader.window == 0)
+    {
+
+        if (recvFlag)
+        {
+            sendto(endpoint, &sendHeader, HEADER_SIZE, 0, (const struct sockaddr *)&dest, sizeof(dest));
             recvFlag = 0;
-            
         }
         alarm(10);
-        recvfrom(endpoint, &recvHeader, sizeof recvHeader ,0, (struct sockaddr *) &sender, (socklen_t *) &length );
-        
-    } 
+        recvfrom(endpoint, &recvHeader, sizeof recvHeader, 0, (struct sockaddr *)&sender, (socklen_t *)&length);
+    }
 
-    
-        return recvHeader; 
-     
-   
- }
-
-  
- 
- 
- 
-
-
+    return recvHeader;
+}
