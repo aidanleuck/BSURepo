5462f8df3dbd8f6e3060dd8bf51137c08b01fd85
aidanleuck <aidan@leucks.com>
2021-11-12 09:56:55 -0700

    debug message

--- CS452/hw4/Scanner.c
+++ CS452/hw4/Scanner.c
@@ -73,6 +73,7 @@ static int inSep(struct file *filp, char cmp)
 extern ssize_t read(struct file *filp, char *buf, size_t charRequested, loff_t *f_pos)
 {
     Scanner* scan = filp->private_data;
+    printk(KERN_INFO, "%s: sep: %s\n charRequested: %d\n input: %s\n sepLength: %d\n inputLength: %d\n", DEVNAME, scan->sep, charRequested, scan->s, scan->sepLength, scan->inputSize);
     char *currentString = kmalloc(sizeof(char) * (charRequested + 1), GFP_KERNEL);
     if(!currentString){
         printk(KERN_ERR, "%s: kmalloc failed", DEVNAME);
